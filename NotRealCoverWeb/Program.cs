015202da03948cf3a92a1f65d99b28e9722ecf06
Jeferson Josue Muñoz Lopez <Jefer10Mil@DESKTOP-EC207QH>
2024-04-30 19:41:58 -0600

    agg Inyecciones

--- NotRealCoverWeb/Program.cs
+++ NotRealCoverWeb/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using NotRealCoverWeb.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<NotRealCoverWebContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("conn")));
+
 
 var app = builder.Build();
 
